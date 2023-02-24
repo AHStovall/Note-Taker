465e1b69a1866290ef28dbe12fce68fa182ac1c7
AHStovall <adamhstovall@gmail.com>
2023-02-24 14:09:00 -0500

    Server.js updated to reflect appropriate porting

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const apiRoutes = require('./Routes/apiRoutes')
 const htmlRoutes = require('./Routes/htmlRoutes')
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
